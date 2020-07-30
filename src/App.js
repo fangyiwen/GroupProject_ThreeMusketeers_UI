4ceaff1f8a1213248cc6cdbaf1d9cec1f05e7ff9
Landon Liu <landonliu@outlook.com>
2020-07-29 20:13:28 -0700

    add react router for 3 components

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import Sites from "./pages/Sites"
 import SingleSite from "./pages/SingleSite"
 import Error from "./pages/Error"
 
+import { Route, Switch } from "react-router-dom"
+
 function App() {
   return (
     <div>
-      <Home></Home>
-      <Sites></Sites>
-      <SingleSite></SingleSite>
-      <Error></Error>
+    <Route exact path="/" component={Home}/>
+    <Route path="/sites" component={Sites}/>
+    <Route path="/single-site" component={SingleSite}/>
+
     </div>);
 }
 
