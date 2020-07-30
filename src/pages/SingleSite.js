e686b86b52dd409e2719fc3a7ac26ce60f24bc31
Landon Liu <landonliu@outlook.com>
2020-07-29 19:49:50 -0700

    add four main components and img sources

---
+++ src/pages/SingleSite.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SingleSite extends Component {
+    render() {
+        return (
+            <div>
+                Hello From Single Site page
+            </div>
+        )
+    }
+}
+
+export default SingleSite
---
+++ src/pages/Sites.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sites = () => {
+    return (
+        <div>
+            Hello from Sites page
+        </div>
+    )
+}
+
+export default Sites
