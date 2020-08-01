26d5bd7a9000c2695e9d9ecba2d927a04766923c
Peng <hao.pe@husky.neu.edu>
2020-08-01 22:13:00 +0800

    Add static map in different sites

--- src/components/SiteContainer.jsx
+++ src/components/SiteContainer.jsx
@@ -10,21 +10,33 @@ export default class SiteContainer extends Component {
         sites: [
             {
                 photoLink: pic1,
+                mapLink: "https://maps.googleapis.com/maps/api/staticmap?center=The+Great+Pyramid+of+Giza&zoom=16&size=500x500&maptype=satellite\n" +
+                    "&markers=color:blue%7Clabel:T%7C29.9792345,31.1320132\n" +
+                    "&key=AIzaSyB2GEsGixSm9xrawPeXgzMmfoGjCkRS2WA",
                 title: "Pyramid",
                 info: "Lorem Ipsum"
             },
             {
                 photoLink: pic2,
+                mapLink: "https://maps.googleapis.com/maps/api/staticmap?center=Great+Wall+of+China&zoom=13&size=500x500&maptype=satellite\n" +
+                    "&markers=color:blue%7Clabel:T%7C40.4322953,116.5650018\n" +
+                    "&key=AIzaSyB2GEsGixSm9xrawPeXgzMmfoGjCkRS2WA",
                 title: "The Great Wall",
                 info: "Lorem Ipsum"
             },
             {
                 photoLink: pic3,
+                mapLink: "https://maps.googleapis.com/maps/api/staticmap?center=Taj+Mahal&zoom=16&size=500x500&maptype=satellite\n" +
+                    "&markers=color:blue%7Clabel:T%7C27.1751448,78.0399535\n" +
+                    "&key=AIzaSyB2GEsGixSm9xrawPeXgzMmfoGjCkRS2WA",
                 title: "Taj Mahal",
                 info: "Lorem Ipsum"
             },
             {
                 photoLink: pic4,
+                mapLink: "https://maps.googleapis.com/maps/api/staticmap?center=Colosseum&zoom=16&size=500x500&maptype=satellite\n" +
+                    "&markers=color:blue%7Clabel:T%7C41.8906874,12.4909819\n" +
+                    "&key=AIzaSyB2GEsGixSm9xrawPeXgzMmfoGjCkRS2WA",
                 title: "Colosseum",
                 info: "Lorem Ipsum"
             }
@@ -40,10 +52,8 @@ export default class SiteContainer extends Component {
                             <article key={index} className="site-container">
                                 <span>
                                     <img src={item.photoLink} alt="photos" />\
-                                    <!-- Google static map
-                                    <img src={"https://maps.googleapis.com/maps/api/staticmap?center=The+Great+Pyramid+of+Giza&zoom=13&size=600x300&maptype=roadmap\n" +
-                                    "&markers=color:blue%7Clabel:S%7C40.702147,-74.015794\n" +
-                                    "&key=AIzaSyB2GEsGixSm9xrawPeXgzMmfoGjCkRS2WA"} /> -->
+
+                                    <img src={item.mapLink} />
                                 </span>
                                 <h6>{item.title}</h6>
                                 <p>{item.info}</p>
