5e52baf144d4458a484941bd8e6ce09378b49b67
Landon Liu <landonliu@outlook.com>
2020-08-04 12:59:01 -0700

    update data json and Map component for new format

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,37 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { GoogleMap, Marker, InfoWindow } from "react-google-maps";
-import * as parkData from "../data/skateboard-parks.json";
+import * as siteData from "../data/unesco-sites.json";
 import mapStyles from "../mapStyles";
 
 export default function Map() {
     const [selectedPark, setSelectedPark] = useState(null);
 
-    useEffect(() => {
-        const listener = e => {
-          if (e.key === "Escape") {
-            setSelectedPark(null);
-          }
-        };
-        window.addEventListener("keydown", listener);
+    // useEffect(() => {
+    //     const listener = e => {
+    //       if (e.key === "Escape") {
+    //         setSelectedPark(null);
+    //       }
+    //     };
+    //     window.addEventListener("keydown", listener);
     
-        return () => {
-          window.removeEventListener("keydown", listener);
-        };
-      }, []);
+    //     return () => {
+    //       window.removeEventListener("keydown", listener);
+    //     };
+    //   }, []);
     
     return (
         <GoogleMap
-            defaultZoom={10}
-            defaultCenter={{ lat: 45.4211, lng: -75.6903 }} // location upon loading
+            defaultZoom={3}
+            defaultCenter={{ lat: 32.4279, lng: 53.6880 }} // location upon loading
             defaultOptions={{ styles: mapStyles }}
         >   
             {/* add markers from database */}
-            {parkData.features.map(park => (
+            {siteData.features.map(park => (
                 <Marker
                     key={park.properties.PARK_ID}
                     position={{
-                        lat: park.geometry.coordinates[1],
-                        lng: park.geometry.coordinates[0]
+                        lat: park.geometry.coordinates[0],
+                        lng: park.geometry.coordinates[1]
                     }}
                     onClick={() => {
                         setSelectedPark(park);
@@ -50,13 +50,13 @@ export default function Map() {
                         setSelectedPark(null);
                     }}
                     position={{
-                        lat: selectedPark.geometry.coordinates[1],
-                        lng: selectedPark.geometry.coordinates[0]
+                        lat: selectedPark.geometry.coordinates[0],
+                        lng: selectedPark.geometry.coordinates[1]
                     }}
                 >
                     <div>
                         <h4>{selectedPark.properties.NAME}</h4>
-                        <p>{selectedPark.properties.DESCRIPTIO}</p>
+                        <p>{selectedPark.properties.DESCRIPTION}</p>
                     </div>
                 </InfoWindow>
             )}
