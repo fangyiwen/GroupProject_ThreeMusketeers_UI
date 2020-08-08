8167fb2abb5d350c4cc4ed123c93ced0f58dd463
Peng <hao.pe@husky.neu.edu>
2020-08-08 22:24:49 +0800

    Add list all sites component

---
+++ src/placesList/Containers/SearchResultContainer.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+import API from '../../API'
+import SitesContainer from './SitesContainer'
+import LoadingContainer from './LoadingContainer'
+import SearchBar from './SearchBar'
+
+class SearchResultsContainer extends React.PureComponent {
+    state = {
+        sites: [],
+        searchCriteria: '',
+        loading: true
+    }
+
+    getSearchResultsBasedOnURL = () => {
+        if (this.props.match.url.split('/').includes('search_by_tag')) {
+            let split_url = this.props.match.url.split('/')
+            let tag = split_url[split_url.length - 1]
+            API.searchByTag(tag).then(data =>
+                this.setState({
+                    sites: data,
+                    searchCriteria: tag,
+                    loading: false
+                })
+            )
+
+        } else {
+            let url = this.props.match.url
+            API.search(url).then(data =>
+                this.setState({
+                    sites: data,
+                    searchCriteria: url
+                        .split('=')[1]
+                        .split('+')
+                        .join(' '),
+                    loading: false
+                })
+            )
+        }
+        window.scrollTo(0, 0)
+    }
+
+    componentDidMount () {
+        this.getSearchResultsBasedOnURL()
+    }
+
+    componentDidUpdate(prevProps){
+        if(prevProps.match.url !== this.props.match.url) {
+            this.setState({
+                loading: true
+            })
+            this.getSearchResultsBasedOnURL()
+        }
+    }
+
+    render () {
+        const {
+            bucketlist,
+            visited,
+            addBucketlistSiteToState,
+            addVisitedSiteToState,
+            removeBucketlistSiteFromState,
+            removeVisitedSiteFromState,
+            signin
+        } = this.props
+
+        const { sites, searchCriteria, loading } = this.state
+
+        return (
+            <div className='page-content-container'>
+                {loading ? (
+                    <LoadingContainer />
+                ) : (
+                    <div>
+                        <div className='search-results-container-headers'>
+                            <div className='primary-header-container'>
+                                {`Displaying results for '${searchCriteria}'`}
+                            </div>
+                            <SearchBar {...this.routerProps} />
+                        </div>
+
+                        <SitesContainer
+                            signin={signin}
+                            sites={sites}
+                            visited={visited}
+                            bucketlist={bucketlist}
+                            addBucketlistSiteToState={addBucketlistSiteToState}
+                            addVisitedSiteToState={addVisitedSiteToState}
+                            removeBucketlistSiteFromState={removeBucketlistSiteFromState}
+                            removeVisitedSiteFromState={removeVisitedSiteFromState}
+                        />
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
+
+export default withRouter(SearchResultsContainer)
---
+++ src/placesList/List.js
@@ -0,0 +1,246 @@
+import React from 'react'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+
+import './List.css'
+
+import API from '../API'
+import SiteContainer from './containers/SiteContainer'
+import sampleSites from './data/sampleSites'
+import HomeContainer from "./containers/HomeContainer";
+import SearchResultsContainer from "./containers/SearchResultContainer";
+import SavedContainer from "./containers/SavedContainer";
+
+
+class List extends React.Component {
+    state = {
+        user_id: '',
+        first_name: '',
+        bucketlist: [],
+        visited: [],
+        europe_and_north_america: [],
+        latin_america_and_the_caribbean: [],
+        africa: [],
+        asia_and_the_pacific: [],
+        arab_states: []
+    }
+
+    componentDidMount () {
+        const token = localStorage.getItem('token')
+        if (token) {
+            API.validate()
+                .then(data => {
+                    if (data.error) throw Error(data.error)
+                    this.signin(data)
+                })
+                .catch(error => console.log(error))
+        }
+        this.getSampleSites()
+    }
+
+    signin = user => {
+        this.setState(
+            {
+                user_id: user.id,
+                first_name: user.first_name
+            },
+            () => this.getBucketlistAndVisited(user.id)
+        )
+        localStorage.setItem('token', user.token)
+    }
+
+    getBucketlistAndVisited = async user_id => {
+        const bucketlist = await API.getBucketlist(user_id)
+        const visited = await API.getVisited(user_id)
+        this.setState({
+            bucketlist,
+            visited
+        })
+    }
+
+    getSampleSites = async () => {
+        const sample_site_data = {}
+        for (const key in sampleSites) {
+            sample_site_data[key] = await this.getTheseSites(sampleSites[key])
+        }
+        this.setState(sample_site_data)
+    }
+
+    getTheseSites = siteIds => Promise.all(siteIds.map(API.getSite))
+
+    addBucketlistSiteToState = site => {
+        this.setState({
+            ...this.state,
+            bucketlist: [...this.state.bucketlist, site]
+        })
+    }
+
+    removeBucketlistSiteFromState = site_id => {
+        let filteredBucketlist = this.state.bucketlist.filter(
+            site => site.id !== site_id
+        )
+        this.setState({
+            ...this.state,
+            bucketlist: filteredBucketlist
+        })
+    }
+
+    addVisitedSiteToState = site => {
+        let filteredBucketlist = this.state.bucketlist.filter(s => s.id !== site.id)
+        this.setState({
+            ...this.state,
+            bucketlist: filteredBucketlist,
+            visited: [...this.state.visited, site]
+        })
+    }
+
+    removeVisitedSiteFromState = site_id => {
+        let filteredVisited = this.state.visited.filter(site => site.id !== site_id)
+        this.setState({
+            ...this.state,
+            visited: filteredVisited
+        })
+    }
+
+    signout = () => {
+        this.setState({
+            user_id: '',
+            first_name: '',
+            bucketlist: [],
+            visited: []
+        })
+        localStorage.removeItem('token')
+    }
+
+    render () {
+        const {
+            signin,
+            signout,
+            addBucketlistSiteToState,
+            addVisitedSiteToState,
+            removeBucketlistSiteFromState,
+            removeVisitedSiteFromState
+        } = this
+
+        const {
+            user_id,
+            first_name,
+            bucketlist,
+            visited,
+            europe_and_north_america,
+            latin_america_and_the_caribbean,
+            africa,
+            asia_and_the_pacific,
+            arab_states
+        } = this.state
+
+        return (
+            <Router>
+                <div className='List'>
+                    <Switch>
+                        <Route
+                            path='/list'
+                            render={routerProps => (
+                                <HomeContainer
+                                    {...routerProps}
+                                    user_id={user_id}
+                                    first_name={first_name}
+                                    signin={signin}
+                                    signout={signout}
+                                    europe_and_north_america={europe_and_north_america}
+                                    latin_america_and_the_caribbean={
+                                        latin_america_and_the_caribbean
+                                    }
+                                    africa={africa}
+                                    asia_and_the_pacific={asia_and_the_pacific}
+                                    arab_states={arab_states}
+                                    bucketlist={bucketlist}
+                                    visited={visited}
+                                    addBucketlistSiteToState={addBucketlistSiteToState}
+                                    addVisitedSiteToState={addVisitedSiteToState}
+                                    removeBucketlistSiteFromState={removeBucketlistSiteFromState}
+                                    removeVisitedSiteFromState={removeVisitedSiteFromState}
+                                />
+                            )}
+                        />
+                        <Route
+                            path='/list/search/:query'
+                            render={routerProps => (
+                                <SearchResultsContainer
+                                    {...routerProps}
+                                    user_id={user_id}
+                                    first_name={first_name}
+                                    signin={signin}
+                                    signout={signout}
+                                    visited={visited}
+                                    bucketlist={bucketlist}
+                                    addBucketlistSiteToState={addBucketlistSiteToState}
+                                    addVisitedSiteToState={addVisitedSiteToState}
+                                    removeBucketlistSiteFromState={removeBucketlistSiteFromState}
+                                    removeVisitedSiteFromState={removeVisitedSiteFromState}
+                                />
+                            )}
+                        />
+                        <Route
+                            path='/list/search_by_tag/:tag'
+                            render={routerProps => (
+                                <SearchResultsContainer
+                                    {...routerProps}
+                                    user_id={user_id}
+                                    first_name={first_name}
+                                    signin={signin}
+                                    signout={signout}
+                                    visited={visited}
+                                    bucketlist={bucketlist}
+                                    addBucketlistSiteToState={addBucketlistSiteToState}
+                                    addVisitedSiteToState={addVisitedSiteToState}
+                                    removeBucketlistSiteFromState={removeBucketlistSiteFromState}
+                                    removeVisitedSiteFromState={removeVisitedSiteFromState}
+                                />
+                            )}
+                        />
+
+                        <Route
+                            path='/list/sites/:id'
+                            render={routerProps => (
+                                <SiteContainer
+                                    {...routerProps}
+                                    bucketlist={bucketlist}
+                                    visited={visited}
+                                    user_id={user_id}
+                                    first_name={first_name}
+                                    signin={signin}
+                                    signout={signout}
+                                    addBucketlistSiteToState={addBucketlistSiteToState}
+                                    addVisitedSiteToState={addVisitedSiteToState}
+                                    removeBucketlistSiteFromState={removeBucketlistSiteFromState}
+                                    removeVisitedSiteFromState={removeVisitedSiteFromState}
+                                />
+                            )}
+                        />
+
+                        <Route
+                            path='/list/users/:id/saved'
+                            component={routerProps => (
+                                <SavedContainer
+                                    {...routerProps}
+                                    user_id={this.state.user_id}
+                                    bucketlist={bucketlist}
+                                    visited={visited}
+                                    addBucketlistSiteToState={addBucketlistSiteToState}
+                                    addVisitedSiteToState={addVisitedSiteToState}
+                                    removeBucketlistSiteFromState={removeBucketlistSiteFromState}
+                                    removeVisitedSiteFromState={removeVisitedSiteFromState}
+                                />
+                            )}
+                        />
+                        <Route
+                            component={() => <div className='not-found'>Page Not Found</div>}
+                        />
+                    </Switch>
+                </div>
+            </Router>
+        )
+    }
+}
+
+export default List
