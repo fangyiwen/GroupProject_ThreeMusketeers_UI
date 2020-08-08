d173db2af5e2048c4c709c28525ffd35e81350a9
Peng <hao.pe@husky.neu.edu>
2020-08-08 22:19:22 +0800

    Add loading container

---
+++ src/placesList/Containers/HomeContainer.js
@@ -0,0 +1,194 @@
+import React from 'react'
+import SitesContainer from './SitesContainer'
+import { Icon } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import LoadingContainer from './LoadingContainer'
+
+class HomeContainer extends React.Component {
+    state = {
+        loading: true
+    }
+
+    componentDidMount () {
+        this.props.africa &&
+        this.setState({
+            loading: false
+        })
+        window.scrollTo(0, 0)
+    }
+
+    render () {
+        const {
+            bucketlist,
+            visited,
+            europe_and_north_america,
+            latin_america_and_the_caribbean,
+            africa,
+            asia_and_the_pacific,
+            arab_states,
+            addBucketlistSiteToState,
+            addVisitedSiteToState,
+            removeBucketlistSiteFromState,
+            removeVisitedSiteFromState,
+            signin
+        } = this.props
+
+        const { loading } = this.state
+
+        return (
+            <div className='page-content-container'>
+                {loading ? (
+                    <LoadingContainer />
+                ) : (
+                    <div>
+                        <div className='home-header-image-container'>
+                            <div className='home-header-content-container-flex'>
+                                <div className='welcome-to'>WELCOME TO EXPLORE</div>
+                                <div className='unesgo-large'>UNESCO</div>
+                            </div>
+                        </div>
+
+                        <div className='sample-sites-container'>
+                            <div className='region-sites-container'>
+                                <div className='primary-header-container'>Explore Africa</div>
+                                <div className='sub-header-container'>
+                                    Wildlife, medinas, and ancient wonders...
+                                </div>
+                                <SitesContainer
+                                    signin={signin}
+                                    sites={africa}
+                                    visited={visited}
+                                    bucketlist={bucketlist}
+                                    addBucketlistSiteToState={addBucketlistSiteToState}
+                                    addVisitedSiteToState={addVisitedSiteToState}
+                                    removeBucketlistSiteFromState={removeBucketlistSiteFromState}
+                                    removeVisitedSiteFromState={removeVisitedSiteFromState}
+                                />
+                                <div className='site-description-link-container'>
+                                    <div className='site-description-link-container-text'>
+                                        <Link to={'/list/region=Africa'}>
+                                            {' '}
+                                            See more from Africa{' '}
+                                            <Icon name='chevron right' size='small' />{' '}
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='region-sites-container'>
+                                <div className='primary-header-container'>
+                                    Explore Arab States
+                                </div>
+                                <div className='sub-header-container'>
+                                    Rich in history and beauty...
+                                </div>
+                                <SitesContainer
+                                    signin={signin}
+                                    sites={arab_states}
+                                    visited={visited}
+                                    bucketlist={bucketlist}
+                                    addBucketlistSiteToState={addBucketlistSiteToState}
+                                    addVisitedSiteToState={addVisitedSiteToState}
+                                    removeBucketlistSiteFromState={removeBucketlistSiteFromState}
+                                    removeVisitedSiteFromState={removeVisitedSiteFromState}
+                                />
+                                <div className='site-description-link-container'>
+                                    <div className='site-description-link-container-text'>
+                                        <Link to={'/list/region=Arab+States'}>
+                                            See more from Arab States{' '}
+                                            <Icon name='chevron right' size='small' />{' '}
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='region-sites-container'>
+                                <div className='primary-header-container'>
+                                    Explore Asia and the Pacific
+                                </div>
+                                <div className='sub-header-container'>
+                                    Rugged landscapes and fascinating history...
+                                </div>
+                                <SitesContainer
+                                    signin={signin}
+                                    sites={asia_and_the_pacific}
+                                    visited={visited}
+                                    bucketlist={bucketlist}
+                                    addBucketlistSiteToState={addBucketlistSiteToState}
+                                    addVisitedSiteToState={addVisitedSiteToState}
+                                    removeBucketlistSiteFromState={removeBucketlistSiteFromState}
+                                    removeVisitedSiteFromState={removeVisitedSiteFromState}
+                                />
+                                <div className='site-description-link-container'>
+                                    <div className='site-description-link-container-text'>
+                                        <Link to={'/list/region=Asia+and+the+Pacific'}>
+                                            {' '}
+                                            See more from Asia and the Pacific{' '}
+                                            <Icon name='chevron right' size='small' />{' '}
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='region-sites-container'>
+                                <div className='primary-header-container'>
+                                    Explore Europe and North America
+                                </div>
+                                <div className='sub-header-container'>
+                                    Historic architecture and stunning landscapes...
+                                </div>
+                                <SitesContainer
+                                    signin={signin}
+                                    sites={europe_and_north_america}
+                                    visited={visited}
+                                    bucketlist={bucketlist}
+                                    addBucketlistSiteToState={addBucketlistSiteToState}
+                                    addVisitedSiteToState={addVisitedSiteToState}
+                                    removeBucketlistSiteFromState={removeBucketlistSiteFromState}
+                                    removeVisitedSiteFromState={removeVisitedSiteFromState}
+                                />
+                                <div className='site-description-link-container'>
+                                    <div className='site-description-link-container-text'>
+                                        <Link to={'/list/region=Europe+and+North+America'}>
+                                            See more from Europe and North America{' '}
+                                            <Icon name='chevron right' size='small' />{' '}
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='region-sites-container'>
+                                <div className='primary-header-container'>
+                                    Explore Latin America and the Caribbean
+                                </div>
+                                <div className='sub-header-container'>
+                                    Biodiversity found no where else...
+                                </div>
+                                <SitesContainer
+                                    signin={signin}
+                                    sites={latin_america_and_the_caribbean}
+                                    visited={visited}
+                                    bucketlist={bucketlist}
+                                    addBucketlistSiteToState={addBucketlistSiteToState}
+                                    addVisitedSiteToState={addVisitedSiteToState}
+                                    removeBucketlistSiteFromState={removeBucketlistSiteFromState}
+                                    removeVisitedSiteFromState={removeVisitedSiteFromState}
+                                />
+                                <div className='site-description-link-container'>
+                                    <div className='site-description-link-container-text'>
+                                        <Link to={'/list/region=Latin+America+and+the+Caribbean'}>
+                                            See more from Latin America and the Caribbean{' '}
+                                            <Icon name='chevron right' size='small' />{' '}
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
+
+export default HomeContainer
